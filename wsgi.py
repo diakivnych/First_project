4bae74eb4e2775933721189352b78dd783a763ae
Dima <d.diakivnych@gmail.com>
2020-04-28 19:26:02 +0300

    third_version

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from person import Person
 
 app = Flask(__name__)
@@ -7,6 +7,7 @@ db = shelve.open('people3')
 
 @app.route('/users/<int:id>')
 def index(id):
+    max_id = int(db['0'].id)
     human = db[str(id)] 
     return render_template("index.html", 
                             first_name = human.first_name,
@@ -15,19 +16,36 @@ def index(id):
                             job = human.job,
                             hobby = human.hobby,
                             id = id,
-                            address = human.address)
+                            address = human.address,
+                            max_id = max_id)
 
 @app.route('/users')
 def all_users():
+    max_id = int(db['0'].id)
     return render_template("all_users.html", 
-                            human = db)
+                            human = db,
+                            max_id = max_id)
 
 @app.route('/')
 def hello():
     return "Hello World"
 
-@app.route('/users/new_user')
+@app.route('/users/new_user', methods=['GET', 'POST'])
 def add_user():
+    if request.method == 'POST':
+        next_id = str(int(db['0'].id) + 1)
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        age = request.form['age']
+        job = request.form['job']
+        hobby = request.form['hobby']
+        human = Person(next_id, first_name, last_name, age, job, hobby)
+        db[next_id] = human
+        db['0']= Person(str(int(db['0'].id) + 1), '', '', '', '', '')
+        #picture = request.files['picture']
+        #picture.save('/home/dima/Study/Flask/Db/static/img100.jpg')
+        return render_template("new_user.html")
+
     return render_template("new_user.html")
 
 @app.route('/users/author')
